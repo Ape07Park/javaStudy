12222aced08b091e53588ca1682e167010d38e95
Ape07Park <ape072399@gmail.com>
2025-05-01 17:47:55 +0900

    first commit

---
+++ src/variable/Var3.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var3 {
+
+    public static void main(String[] args) {
+        // 변수 선언: 메모리 공간을 차지
+
+        int a; // 변수 선언
+
+        a = 10; // 변수 초기화
+        System.out.println(a);
+
+        a = 50; // a에 대입한 값 50으로 변경
+        System.out.println(a);
+    }
+}
