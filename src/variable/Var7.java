e18a28ac1cde1660a23f523ace293e819ff2257a
Ape07Park <ape072399@gmail.com>
2025-05-02 15:48:53 +0900

    변수 끝.

---
+++ src/variable/Var7.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var7 {
+
+    public static void main(String[] args) {
+
+    int a = 100; // 정수
+    double b = 10.5; // 실수
+    boolean c = true; // 불리언 true, false
+    char d = 'A';
+    String e = "Hello Java"; // 문자열(참조 자료형)
+
+        // 100, 10.5와 같이 개발자가 직접 적어넣은 값을
+        // 리터럴(문자, 글자)이라고 한다.
+        // 리터럴은 개발자가 직접 넣은 것이라 고정된 것이고 변화 x
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,17 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+
+        Long num1 = 100000000000L;
+
+        System.out.println(num1);
+
+        boolean bool = true;
+
+        System.out.println(bool);
+    }
+
+
+}
