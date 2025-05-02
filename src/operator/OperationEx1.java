a22ad9d21a5e6f706671b00f067e7316ec778a20
Ape07Park <ape072399@gmail.com>
2025-05-02 17:57:46 +0900

    연산자 끝.

---
+++ src/operator/OperationEx1.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class OperationEx1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        int sum = a + b + c;
+        System.out.println(sum);
+
+        int average = sum / 3;
+        System.out.println(average);
+
+    }
+}
---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+        int score;
+
+        score = 110;
+
+        System.out.println(80 <= score && score <= 100);
+    }
+}
