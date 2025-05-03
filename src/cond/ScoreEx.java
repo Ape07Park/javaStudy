707f3e316a0d021bddb84c6f487a9396d1f83219
Ape07Park <ape072399@gmail.com>
2025-05-03 18:49:15 +0900

    조건문 끝.

---
+++ src/cond/ScoreEx.java
@@ -0,0 +1,24 @@
+package cond;
+
+import java.util.Scanner;
+
+public class ScoreEx {
+    public static void main(String[] args) {
+       int score;
+       Scanner scanner = new Scanner(System.in);
+
+       score = scanner.nextInt();
+
+       if (score >= 90) {
+           System.out.println("A");
+       } else if (score >= 80) {
+           System.out.println("B");
+       } else if (score >= 70) {
+           System.out.println("C");
+       } else if (score >= 60) {
+           System.out.println("D");
+       } else {
+           System.out.println("F");
+       }
+    }
+}
