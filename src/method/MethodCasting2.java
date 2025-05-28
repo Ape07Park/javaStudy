d6a881363b265e6458bb45b88e9a01971bee52ea
SUNGMIN PARK <ape072399@gmail.com>
2025-05-28 13:29:44 +0900

    메소드와 형변환

---
+++ src/method/MethodCasting2.java
@@ -0,0 +1,15 @@
+package method;
+
+public class MethodCasting2 {
+
+    public static void main(String[] args) {
+        int number = 100;
+
+        pringNumber(number);
+    }
+
+    public static void pringNumber(double n) {
+        System.out.println("숫자: " + n);
+    }
+
+}
