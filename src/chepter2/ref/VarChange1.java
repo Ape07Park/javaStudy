94b6849a6a73b2afd3e4cd8de9f7cf753b7f0de9
Ape07Park <ape072399@gmail.com>
2025-06-01 20:42:22 +0900

    변수 대입 작업 중

--- src/chepter2/ref/VarChange1.java
+++ src/chepter2/ref/VarChange1.java
@@ -3,6 +3,24 @@ package chepter2.ref;
 public class VarChange1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int a = 10;
+        int b = a;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        // a 변경
+        a = 20;
+
+        System.out.println("변경 a = 20");
+        System.out.println(a);
+        System.out.println(b);
+
+        // b 변경
+        b = 30;
+        System.out.println("변경 b = 30");
+        System.out.println(a);
+        System.out.println(b);
     }
 }
