2425f478608f5da59955837f7e6d2bcd7547f0d6
Ape07Park <ape072399@gmail.com>
2025-06-06 21:00:07 +0900

    import 듣기

---
+++ src/chepter2/pack/Data.java
@@ -0,0 +1,8 @@
+package chepter2.pack;
+
+public class Data {
+
+    public Data() {
+        System.out.println("패키지 pack에서 생성한 Data");
+    }
+}
---
+++ src/chepter2/pack/PackageMain1.java
@@ -0,0 +1,12 @@
+package chepter2.pack;
+
+
+
+public class PackageMain1 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+
+        chepter2.pack.a.User user = new chepter2.pack.a.User();
+    }
+}
