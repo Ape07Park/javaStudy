287bd7899f09261c1ca3ce008634821100a6d05c
SUNGMIN PARK <ape072399@gmail.com>
2025-06-30 13:33:22 +0900

    super() 듣는 중

--- src/chepter2/extends1/super2/ClassA.java
+++ src/chepter2/extends1/super2/ClassA.java
@@ -1,4 +1,8 @@
 package chepter2.extends1.super2;
 
 public class ClassA {
+
+	public ClassA() {
+		System.out.println("classA 생성자");
+	}
 }
--- src/chepter2/extends1/super2/ClassB.java
+++ src/chepter2/extends1/super2/ClassB.java
@@ -1,4 +1,14 @@
 package chepter2.extends1.super2;
 
-public class ClassB {
+public class ClassB extends ClassA {
+
+	public ClassB(int a) {
+		super(); // 기본 생성자 호출 - 생략 o, 자바가 자동으로 생성해줌
+		System.out.println("ClassB 생성자 a=" + a);
+	}
+
+	public ClassB(int a, int b) {
+		super();
+		System.out.println("ClassB 생성자 a=" + a + " b=" + b );
+	}
 }
