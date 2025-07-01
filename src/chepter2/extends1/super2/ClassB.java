de5a5e711487f0632c1b74913170f97442623d3a
SUNGMIN PARK <ape072399@gmail.com>
2025-07-01 18:27:02 +0900

    super() 끝

--- src/chepter2/extends1/super2/ClassB.java
+++ src/chepter2/extends1/super2/ClassB.java
@@ -2,8 +2,13 @@ package chepter2.extends1.super2;
 
 public class ClassB extends ClassA {
 
+	// public ClassB(int a) {
+	// 	super(); // 기본 생성자 호출 - 생략 o, 자바가 자동으로 생성해줌
+	// 	System.out.println("ClassB 생성자 a=" + a);
+	// }
+
 	public ClassB(int a) {
-		super(); // 기본 생성자 호출 - 생략 o, 자바가 자동으로 생성해줌
+		this(a, 10); // 자기 자신-자기 클래스-의 다른 생성자를 호출: 여기선  ClassB(int a, int b) 호출
 		System.out.println("ClassB 생성자 a=" + a);
 	}
 
--- src/chepter2/extends1/super2/Super2Main.java
+++ src/chepter2/extends1/super2/Super2Main.java
@@ -4,6 +4,9 @@ public class Super2Main {
 
 	public static void main(String[] args) {
 		// ClassC 생성자 호출 - 부모 생성자 호출 및 부모 생성 * 2 - 자식 생성
-		ClassC classC = new ClassC(); // 부모를 먼저 생성하고 그 다음 자식을 생성
+		// ClassC classC = new ClassC(); // 부모를 먼저 생성하고 그 다음 자식을 생성
+
+
+		ClassB classB = new ClassB(10);
 	}
 }
