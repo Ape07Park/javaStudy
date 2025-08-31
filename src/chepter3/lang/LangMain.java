932869948ee432486e895212b6971d5380e99b0f
Ape07Park <ape072399@gmail.com>
2025-08-31 16:54:15 +0900

    불변 객체 듣는 중

---
+++ src/chepter3/lang/LangMain.java
@@ -0,0 +1,8 @@
+package chepter3.lang;
+
+public class LangMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/chepter3/lang/object/immutable/address/MemberMainV2.java
@@ -0,0 +1,25 @@
+package chepter3.lang.object.immutable.address;
+
+public class MemberMainV2 {
+
+	public static void main(String[] args) {
+
+		ImmutableAddress address = new ImmutableAddress("서울");
+
+		MemberV2 memberA = new MemberV2("회원A", address);
+		MemberV2 memberB = new MemberV2("회원B", address);
+
+		// 회원 a,b 모두 처음 주소는 서울
+		System.out.println("memberA = " + memberA);
+		System.out.println("memberB = " + memberB);
+
+		// 회원 b 주소 부산으로 변경
+
+		// *한번 할당하면 값 변경 불가니 생성하면서 값 할당
+		memberB.setAddress(new ImmutableAddress("부산")); // 주소 객체를 새로 생성
+		System.out.println("부산 -> memberB.address");
+		System.out.println("memberA = " + memberA);
+		System.out.println("memberB = " + memberB);
+
+	}
+}
