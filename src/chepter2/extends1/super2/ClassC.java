2a103317aa86f14a4a8206c114814bb49ba34634
James Park <ape072399@gmail.com>
2025-09-16 09:03:53 +0900

    StringBuilder 듣기, 상속 블로그 작성 중

--- src/chepter2/extends1/super2/ClassC.java
+++ src/chepter2/extends1/super2/ClassC.java
@@ -3,6 +3,7 @@ package chepter2.extends1.super2;
 public class ClassC extends ClassB {
 
 	public ClassC() {
+
 		// super(); // ClassB에 기본 생성자 없어서 컴파일 오류 발생
 		super(10, 20);
 		System.out.println("ClassC 생성자");
