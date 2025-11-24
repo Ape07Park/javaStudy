f1aa173c4766c54c5946751e7a2099246c0489f1
Ape07Park <ape072399@gmail.com>
2025-11-24 22:53:07 +0900

    클론 공부 중

--- src/jungsuckbook/lang/clone/CloneEx.java
+++ src/jungsuckbook/lang/clone/CloneEx.java
@@ -5,7 +5,8 @@ public class CloneEx {
 	public static void main(String[] args) {
 
 		Point original = new Point(3, 5);
-		Point copy = (Point) original.clone(); // 얕은 복사로 객체 생성
+
+		Point copy = (Point)original.clone(); // 얕은 복사(값만 복사)로 객체 생성
 		System.out.println(original);
 		System.out.println(copy);
 
