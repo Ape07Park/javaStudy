80273fe053bac1bd421dd30ece4e9086af6a26b3
James Park <ape072399@gmail.com>
2025-12-31 09:04:32 +0900

    time 문제풀이 중

--- src/chepter3/time/test/TestAdjusters.java
+++ src/chepter3/time/test/TestAdjusters.java
@@ -1,5 +1,11 @@
 package chepter3.time.test;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
 public class TestAdjusters {
 
 	public static void main(String[] args) {
@@ -8,6 +14,13 @@ public class TestAdjusters {
 		int month = 1;
 
 		// 코드 작성
+
+		LocalDateTime date = LocalDateTime.of(year, month, 1, 0, 0, 0);
+
+
+		// TODO 요일 추출 필요
+		System.out.println(date.with(TemporalAdjusters.firstDayOfMonth()));
+		System.out.println(date.with(TemporalAdjusters.lastDayOfMonth()));
 
 
 	}
