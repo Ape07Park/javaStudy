3eb861d94e6b2f923b7af25536a4234fc1ea5efe
James Park <ape072399@gmail.com>
2026-01-20 09:01:42 +0900

    중첩 클래스 공부 중

---
+++ src/chepter3/nested/nested/ex2/Network.java
@@ -0,0 +1,25 @@
+package chepter3.nested.nested.ex2;
+
+public class Network {
+
+	public void sendMessage(String text) {
+
+		NetworkMessage networkMessage = new NetworkMessage(text);
+
+		networkMessage.print();
+	}
+
+
+	static class NetworkMessage {
+
+		private String content;
+
+		public NetworkMessage(String content) {
+			this.content = content;
+		}
+
+		public void print() {
+			System.out.println(content);
+		}
+	}
+}
