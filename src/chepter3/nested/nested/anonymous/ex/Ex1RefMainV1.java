b320860bc7241ae37bf18e9a921d30796f512667
James Park <ape072399@gmail.com>
2026-01-22 09:03:59 +0900

    중첩 클래스 공부 중

---
+++ src/chepter3/nested/nested/anonymous/ex/Ex1RefMainV1.java
@@ -0,0 +1,44 @@
+package chepter3.nested.nested.anonymous.ex;
+
+import java.util.Random;
+
+// 정적 중첩 클래스
+public class Ex1RefMainV1 {
+
+	public static void hello(Process process) {
+		System.out.println("프로그램 시작");
+
+		process.run();
+
+		System.out.println("프로그램 종료");
+	}
+
+	static class Dice implements Process {
+
+		@Override
+		public void run() {
+			int randomValue = new Random().nextInt(6) + 1;
+			System.out.println("주사위 = " + randomValue);
+		}
+	}
+
+	static class Sum implements Process {
+
+		@Override
+		public void run() {
+
+			for (int i = 1; i <= 3; i++) {
+				System.out.println("i = " + i);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Process dice = new Dice();
+		Process sum = new Sum();
+		hello(dice);
+		hello(sum);
+	}
+
+}
---
+++ src/chepter3/nested/nested/anonymous/ex/Process.java
@@ -0,0 +1,6 @@
+package chepter3.nested.nested.anonymous.ex;
+
+public interface Process {
+
+	void run();
+}
