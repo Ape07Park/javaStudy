00550a173ba7b55a81eee3569c39df2c557dc0b9
Optimist Park <ape072399@gmail.com>
2026-02-23 08:32:57 +0900

    제네릭 2 문제 풀이 끝

--- src/chepter4/generic/test/ex3/unit/UnitPrinter.java
+++ src/chepter4/generic/test/ex3/unit/UnitPrinter.java
@@ -3,15 +3,16 @@ package chepter4.generic.test.ex3.unit;
 public class UnitPrinter {
 
 	// 제네릭 메서드
-	public static void printV1(BioUnit unit) {
+	// 타입 추론 과정 필요
+	public static <T extends BioUnit> void printV1(Shuttle<T> shuttle) {
 
-
+		shuttle.showInfo();
 	}
 
 	// 와일드 카드
-	public static void printV2(BioUnit unit) {
+	public static void printV2(Shuttle<?> shuttle) {
 
-
+		shuttle.showInfo();
 	}
 
 
