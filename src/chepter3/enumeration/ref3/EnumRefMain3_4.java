d07672c456008105d15964468d5fabad1da394aa
James Park <ape072399@gmail.com>
2026-05-09 17:11:11 +0900

    이넘 강의 듣는 중

--- src/chepter3/enumeration/ref3/EnumRefMain3_4.java
+++ src/chepter3/enumeration/ref3/EnumRefMain3_4.java
@@ -1,19 +1,21 @@
 package chepter3.enumeration.ref3;
 
-import chepter3.enumeration.ex2.DiscountService;
-
 public class EnumRefMain3_4 {
 
-	public static void main(String[] args) {
-		int price = 10000;
-		Grade[] grades = Grade.values();
-		for (Grade grade : grades) {
-			printDiscount(grade, price);
-		}
 
-	}
+    public static void main(String[] args) {
 
-	private static void printDiscount(Grade grade, int price) {
-		System.out.println("등급 이름 : " + grade.name() + " 등릅의 할인 금액: " + grade.discount(price));
-	}
+        int price = 10000;
+
+        Grade [] grades = Grade.values();
+
+        for (Grade grade : grades) {
+            printDiscount(grade, price);
+        }
+
+    }
+
+    private static void printDiscount(Grade grade, int price) {
+        System.out.println("등급은 "  + grade.name() + " 할인 가격은 " + grade.discount(price));
+    }
 }
--- src/chepter3/enumeration/ref3/Grade.java
+++ src/chepter3/enumeration/ref3/Grade.java
@@ -3,22 +3,23 @@ package chepter3.enumeration.ref3;
 public enum Grade {
 
 	BASIC(10),
-	GOLD(20),
-	DIAMOND(30);
+    GOLD(20),
+    DIAMOND(30);
+
 
 	private final int discountPercent;
 
-	Grade(int discountPercent) {
-		this.discountPercent = discountPercent;
-	}
+    Grade(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 
-	public int getDiscountPercent() {
+    public int getDiscountPercent() {
 		return discountPercent;
 	}
-	
-	// 할인율 계산을 위해 필드 discountPercent가 외부로 노출되는 건 객체지향 x
-	// 따라서 캡슐화 원칙에 따라 스스로 할인율 계산 후 값만 리턴
-	public int discount(int price) {
-		return price * (discountPercent / 100);
-	}
+
+    // 할인율 계산을 위해 필드 discountPercent가 외부로 노출되는 건 객체지향 x
+    // 따라서 캡슐화 원칙에 따라 스스로 할인율 계산 후 값만 리턴
+    public int discount(int price) {
+    	return price * discountPercent / 100;
+    }
 }
