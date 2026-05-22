31e0acced46f9d0628cdc2fa081e2a27c5361ed9
James Park <ape072399@gmail.com>
2026-05-22 09:16:44 +0900

    time 강의 듣는 중

---
+++ src/chepter3/time/IsSupportedMain2.java
@@ -0,0 +1,24 @@
+package chepter3.time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportedMain2 {
+
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+       boolean supported =  now.isSupported(ChronoField.SECOND_OF_MINUTE);
+
+        // int minute = now.get(ChronoField.SECOND_OF_MINUTE);
+
+        System.out.println(supported);
+
+        if(supported) {
+            int minute = now.get(ChronoField.SECOND_OF_MINUTE);
+            System.out.println(minute);
+        }
+
+
+    }
+}
