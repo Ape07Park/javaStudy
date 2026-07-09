52368e48b8d6bb5d0a4a48e58ba55f2feebfcbac
James Park <ape072399@gmail.com>
2026-07-10 08:58:49 +0900

    제네릭 공부 중

--- src/chepter4/generic/ex1/IntegerBox.java
+++ src/chepter4/generic/ex1/IntegerBox.java
@@ -1,4 +1,14 @@
 package chepter4.generic.ex1;
 
 public class IntegerBox {
+
+    private Integer value;
+
+    public Integer get() {
+        return value;
+    }
+
+    public void set(Integer value) {
+        this.value = value;
+    }
 }
---
+++ src/chepter4/generic/ex1/StringBox.java
@@ -0,0 +1,14 @@
+package chepter4.generic.ex1;
+
+public class StringBox {
+
+    private String value;
+
+    public String get() {
+        return value;
+    }
+
+    public void set(String value) {
+        this.value = value;
+    }
+}
